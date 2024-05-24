a8a34ba8439e105f24d085b1e0f24d0058c4be88
Documenter.jl <documenter@juliadocs.github.io>
2024-05-24 09:07:36 +0000

    build based on cbeebef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FASolverAvalanche","category":"page"},{"location":"#FASolverAvalanche","page":"Home","title":"FASolverAvalanche","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FASolverAvalanche.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FASolverAvalanche]","category":"page"}]
+}
